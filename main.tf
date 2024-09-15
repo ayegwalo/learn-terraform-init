665f19efbec34ffd16564175647836433e47baec
Christopher <chris.relaxier@gmail.com>
2024-09-15 22:16:17 +0400

    Updating module arguments

--- main.tf
+++ main.tf
@@ -38,8 +38,11 @@ module "hello" {
   source  = "joatmon08/hello/random"
   version = "6.0.0"
 
-  hello        = "World"
-  second_hello = random_pet.instance.id
+  hellos = {
+    hello        = random_pet.instance.id
+    second_hello = "World"
+  }
 
-  secret_key = "secret"
+  some_key = "secret"
 }
+
